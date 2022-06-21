9258842ca3f073f4fb5936d56ee0f692d7a7b68e
masahiro.yamane <masahiro.yamane@infcurion.com>
2022-06-21 18:40:51 +0900

    修正

--- lambda_function.py
+++ lambda_function.py
@@ -1,15 +1,30 @@
+import pyodbc
 import json
 import logging
+import rds_config
+import s3_config
+import sys
+import boto3
 
 #log settings
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 #rds settings
-rds_host  = rds_config.db_host
-name = rds_config.db_username
-password = rds_config.db_password
+db_host  = rds_config.db_host
+db_port = rds_config.port
 db_name = rds_config.db_name
+db_user = rds_config.db_username
+db_password = rds_config.db_password
+
+#create rds connection
+try:
+    constr=pyodbc.connect("DRIVER={{/msodbcsql17/lib64/libmsodbcsql-17.3.so.1.1}};SERVER={0};PORT={1};DATABASE={2};UID={3};PWD={4}".format(db_host,db_port,db_name,db_user,db_password))
+except pyodbc.Error as e:
+    logger.error("ERROR: Unexpected error: Could not connect to SQLServer instance.")
+    logger.error(e)
+    sys.exit()
+logger.info("SUCCESS: Connection to RDS SQLServer instance succeeded")
 
 #s3 setting
 s3 = boto3.resource('s3')
