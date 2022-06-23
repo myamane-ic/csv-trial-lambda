62534d716922af9127d59d6f6740536d440dca71
masahiro.yamane <masahiro.yamane@infcurion.com>
2022-06-23 11:02:02 +0900

    INSERTまでDONE

--- lambda_function.py
+++ lambda_function.py
@@ -5,6 +5,8 @@ import rds_config
 import s3_config
 import sys
 import boto3
+import io
+import csv
 import datetime
 
 #log settings
@@ -43,9 +45,6 @@ def lambda_handler(event, context):
     taskId = event['taskId']
     print(taskId)
 
-    #SQL開始
-    dt_now = datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')
-    logger.info("SQL Start: "+dt_now)
     with conn.cursor() as cur:
         # 1.taskIdを基に親レコード検索
         sql = 'select * from task where id = \'' + taskId + '\''
@@ -62,12 +61,39 @@ def lambda_handler(event, context):
     object = bucket.Object(fileName)
     response = object.get()
     body = response['Body'].read().decode(src_file_encoding)
-    
     print(body)
+    
     # 4.ファイル内容をクラスに置き換え
+    st = io.StringIO()
+    st.write(body)
+    st.seek(0)
+    csv_f =csv.reader(st)
+    
+    taskDatas=[] #空List
+    rowNumber = -1 #Csv行数カウント
+    for row in csv_f: #rowはList
+        rowNumber += 1
+        if(rowNumber == 0):
+            continue #最初の行はスキップ
+        #クラス化
+        taskDatas.append(TaskDetailCsvModel(row[0]))
+    #csv行数確認
+    print(rowNumber)
+    
     # 5.以下ループ
-    # 5.1 子テーブルに登録
+    with conn.cursor() as cur:
+        for taskData in taskDatas:
+            # 5.1 子テーブルに登録
+            sql = 'insert into task_detail (task_id, content) values (' + str(taskId) + ', \'' + taskData.content + '\')'
+            print(sql)
+            cur.execute(sql)
+        conn.commit()
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': json.dumps('Lambda Process Finished!')
     }
+
+#csvデータクラス
+class TaskDetailCsvModel:
+    def __init__(self, content):
+        self.content = content
